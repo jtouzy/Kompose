dd5ef88b7c8498a577339eb04d56338dcfa0d31f
Jérémy Touzy <jtouzy2@gmail.com>
2020-01-24 13:40:20 +0100

    Improve characters iOS layout + add base child view

---
+++ Examples/DemoApp/iosApp/DemoApp/Modules/CharacterProfile/CharacterProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterProfileView.swift
+//  DemoApp
+//
+//  Created by Jérémy TOUZY on 24/01/2020.
+//  Copyright © 2020 jtouzy. All rights reserved.
+//
+
+import SwiftUI
+
+//
+// MARK: View Layout
+//
+struct CharacterProfileView: View {
+    var body: some View {
+        VStack {
+            Text("Profile view")
+        }
+    }
+}
--- Examples/DemoApp/iosApp/DemoApp/Modules/MarvelCharacters/MarvelCharactersView.swift
+++ Examples/DemoApp/iosApp/DemoApp/Modules/MarvelCharacters/MarvelCharactersView.swift
@@ -9,37 +9,52 @@
 import SwiftUI
 import shared
 
-extension String: Identifiable {
-    public var id: String {
-        return self
-    }
-}
-
+//
+// MARK: View Layout
+//
 struct MarvelCharactersView: View {
     let presenter: MarvelCharactersPresenter
     @ObservedObject var store: ObservableStore<MarvelCharactersViewState>
     var viewState: MarvelCharactersViewState { store.state }
 
     var body: some View {
-        VStack {
-            containedView()
-        }.onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
-                self.presenter.loadCharaters()
-            })
+        NavigationView {
+            VStack {
+                dynamicContainedView()
+            }
+            .navigationBarTitle("Marvel Characters")
         }
+        .onAppear { self.onViewAppear() }
     }
 
-    func containedView() -> AnyView {
+    func dynamicContainedView() -> AnyView {
         if viewState is Loading {
             return AnyView(Text("Loading"))
         } else if let contentViewState = viewState as? Content {
             return AnyView(
                 List(contentViewState.characters) { character in
-                    Text(character)
+                    NavigationLink(destination: CharacterProfileAssembler.assemble()) {
+                        HStack {
+                            Image(systemName: "heart.fill")
+                            Text(character)
+                        }
+                        .padding()
+                    }
                 }
+                .listStyle(GroupedListStyle())
             )
         }
         return AnyView(Text("Unrecognized state"))
+    }
+}
+
+//
+// MARK: Events
+//
+extension MarvelCharactersView {
+    func onViewAppear() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+            self.presenter.loadCharaters()
+        })
     }
 }
